14fc3f152de4744fa23100ac48344077a4c0e607
flaviotulioalmeida <138700144+flaviotulioalmeida@users.noreply.github.com>
2023-11-01 09:14:28 -0300

    Atualizando o main

--- Main.java
+++ Main.java
@@ -1,45 +1,13 @@
 package view;
 
+import model.Usuario;
+
 import java.io.*;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-        File file = new File("teste.txt");
-
-
-        //Criar arquivo
-        if(!file.exists()){
-            try {
-                file.createNewFile();
-            }catch (IOException exception){
-                System.out.println("Falha ao criar arquivo");
-            }
-        }
-
-//      List<String> lista = List.of("João", "Maria", "Pedro");
-
-        //Escrever no arquivo
-//        try {
-//            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(file)
-//            );
-//            out.writeObject(lista);
-//        }catch(IOException exception){
-//            System.out.println("Falha ao escrever no arquivo");
-//        }
-
-        //Ler do arquivo
-        try{
-            ObjectInputStream in = new ObjectInputStream(new FileInputStream(file)
-            );
-            List<String> lista = (List<String>) in.readObject();
-            System.out.println(lista);
-        }catch (IOException exception){
-            System.out.println("Falha ao ler arquivo");
-        }catch (ClassNotFoundException e){
-            throw new RuntimeException(e);
-        }
 
     }
 }
